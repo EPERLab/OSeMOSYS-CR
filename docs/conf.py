5094df80baaad7389576a22026fba9353e4d5ab2
guidogz <46365369+guidogz@users.noreply.github.com>
2020-02-06 11:56:52 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -189,5 +189,5 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
 
-def setup(app):
-    app.add_stylesheet('theme_overrides.css')
+#def setup(app):
+ #   app.add_stylesheet('theme_overrides.css')
