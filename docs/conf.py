1979f8a907653aadec77d6dff7cec10e45c3f21c
jamangulop <54330301+jamangulop@users.noreply.github.com>
2020-02-25 11:40:14 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -50,8 +50,6 @@ napoleon_google_docstring = False
 napoleon_use_param = False
 napoleon_use_ivar = True
 
-imgmath_font_size = 9
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
