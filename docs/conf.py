fb302a798e642fc7cac393d4eb437dafa1530e81
jamangulop <54330301+jamangulop@users.noreply.github.com>
2020-02-25 10:47:54 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -191,4 +191,3 @@ intersphinx_mapping = {'https://docs.python.org/': None}
 
 def setup(app):
     app.add_stylesheet('theme_overrides.css')
-    app.add_config_value('imgmath_font_size', 5, 'html')
